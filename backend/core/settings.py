92431c9d1ca66723ed886f5b090f11ff8c764f78
1 <ganz67@mail.ru>
2025-07-22 15:34:50 +0300

    Create settings file

---
+++ backend/core/settings.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DataBaseSettings(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 5
+    max_overflow: int = 10
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix='APP__',
+        env_nested_delimiter='__',
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra='ignore',
+        populate_by_name=True
+    )
+    db: DataBaseSettings
+    
+
+settings = Settings()
+
