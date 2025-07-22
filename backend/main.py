e3e8b89644890f60444086573d967063d75d1637
1 <ganz67@mail.ru>
2025-07-22 15:17:56 +0300

    Created main.py file

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from .api import router as api_router
+
+app = FastAPI(
+    title='Медецинский Урологический Центр API',
+    description='API для системы записи на прием в урологическую клинику',
+    version='1.0.0',
+    responses=ORJSONResponse,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(
+    api_router
+)
