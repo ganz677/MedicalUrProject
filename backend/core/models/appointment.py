5f658de2fb312e61e296ad04030f15adcde9a432
1 <ganz67@mail.ru>
2025-07-22 17:37:40 +0300

    Created appointment model for db

---
+++ backend/core/models/appointment.py
@@ -0,0 +1,65 @@
+from sqlalchemy import (
+    String,
+    Boolean,
+    Text
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column
+)
+
+from .base import Base
+from .mixins.id_mixin import IDMixin
+from .mixins.timestamp_mixin import TimeStampMixin
+
+class Appointment(IDMixin, TimeStampMixin, Base):
+    first_name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+        comment='Имя пациента'
+    )
+    last_name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+        comment='Фамилия пациента'
+    )
+    phone_number: Mapped[str] = mapped_column(
+        String(20)
+        nullable=False,
+        comment='Номер телефона пациента'
+    )
+    email: Mapped[str] = mapped_column(
+        String(255),
+        nullable=True,
+        comment='Email пациента'
+    )
+    age: Mapped[str] = mapped_column(
+        String(20),
+        nullable=False,
+        comment='Возрастная группа'
+    )
+    preferred_time: Mapped[str] = mapped_column(
+        String(50),
+        nullable=True,
+        comment='Предпочтительное время'
+    ) 
+    message: Mapped[str] = mapped_column(
+        Text,
+        nullable=True,
+        comment='Дополнительное сообщение'
+    )
+    privacy_consent: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable=False,
+        default=True,
+        comment='Согласие на обработку'
+    )
+    status: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False,
+        default='new',
+        comment='Статус записи',
+    )
+    
+    def __repr__(self):
+        return f"<Appointment(id={self.id}, name='{self.first_name} {self.last_name}', phone='{self.phone}')>"
