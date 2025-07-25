1bf2dd0eca9fee7cc5c9013b41b76c47eff23415
1 <ganz67@mail.ru>
2025-07-25 17:38:35 +0300

    include admin_router

--- backend/api/v1/__init__.py
+++ backend/api/v1/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from .main_page.routes import router as main_page_router
+from .admin_page.routes import router as auth_router
 
 
 router = APIRouter(
@@ -7,3 +8,4 @@ router = APIRouter(
 )
 
 router.include_router(main_page_router)
+router.include_router(auth_router)
